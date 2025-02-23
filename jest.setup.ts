c287c0e66fa185ded62c522911a023fefe0c59da
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-02-23 13:56:34 -0800

    ci: fixing where the archived static build lives and how it is pulled

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,15 +1,1 @@
-import '@testing-library/jest-dom';
-
-beforeAll(() => {
-  // Check if the document already has an html element, otherwise create it
-  if (!document.documentElement) {
-    const html = document.createElement('html');
-    document.appendChild(html);
-  }
-
-  // Check if the document already has a body element, otherwise create it
-  if (!document.body) {
-    const body = document.createElement('body');
-    document.documentElement?.appendChild(body);
-  }
-});
+import "@testing-library/jest-dom";
