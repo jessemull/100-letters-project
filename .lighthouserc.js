31f882b4eef520b6a3ffab86307decd076ec8b31
Jesse Mull <jessemull@gmail.com>
2025-02-23 19:55:47 -0800

    Lighthouse in ci (#24)
    * test: added ability to run lighthouse in dev mode locally and fixed production build flag
    
    * chore: remove production in parens from github actions file
    
    * ci: added step to perform lighthouse and roll back
    
    * fix: somehow missed some text into workflow
    
    ---------
    
    Co-authored-by: Jesse Mull <jessemull@Jesses-Laptop.local>

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,11 +1,15 @@
+const execa = require('execa');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = {
   ci: {
     assert: {
       assertions: {
-        'categories:performance': ['error', { minScore: 0.9 }],
+        'categories:performance': ['error', { minScore: 0.8 }],
         'categories:accessibility': ['error', { minScore: 0.9 }],
         'categories:seo': ['error', { minScore: 0.9 }],
-        'categories:best-practices': ['error', { minScore: 0.9 }]
+        'categories:best-practices': ['error', { minScore: 0.9 }],
       },
     },
     collect: {
@@ -13,7 +17,7 @@ module.exports = {
       startServer: async () => {
         await execa('npm', ['run', 'dev'], { stdio: 'inherit' });
       },
-      url: ['http://localhost:3000'],
+      url: isProduction ? ['https://onehundredletters.com'] : ['http://localhost:3000'],
     },
     upload: {
       target: 'temporary-public-storage',
