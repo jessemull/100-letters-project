bdcb102622c7188d492a3eac875177b6306e8b3e
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-02-23 21:08:32 -0800

    fix: need dynamic import inside of lighthouse config

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,5 +1,3 @@
-const execa = require('execa');
-
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
@@ -15,6 +13,7 @@ module.exports = {
     collect: {
       numberOfRuns: 1,
       startServer: async () => {
+        const execa = await import('execa');
         await execa('npm', ['run', 'dev'], { stdio: 'inherit' });
       },
       url: isProduction ? ['https://onehundredletters.com'] : ['http://localhost:3000'],
