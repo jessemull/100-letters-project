ce7a1bc2fb2bb6eec4d89b67213d1b6d57126934
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-02-23 19:50:31 -0800

    ci: added step to perform lighthouse and roll back

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,11 +1,15 @@
+const execa = require('execa');
+
+const isProduction = process.env.NODE_ENV === 'production'; // or other environment check
+
 module.exports = {
   ci: {
     assert: {
       assertions: {
-        'categories:performance': ['error', { minScore: 0.9 }],
+        'categories:performance': ['error', { minScore: isProduction ? 0.9 : 0.85 }], // Lower score for staging
         'categories:accessibility': ['error', { minScore: 0.9 }],
         'categories:seo': ['error', { minScore: 0.9 }],
-        'categories:best-practices': ['error', { minScore: 0.9 }]
+        'categories:best-practices': ['error', { minScore: 0.9 }],
       },
     },
     collect: {
@@ -13,10 +17,10 @@ module.exports = {
       startServer: async () => {
         await execa('npm', ['run', 'dev'], { stdio: 'inherit' });
       },
-      url: ['http://localhost:3000'],
+      url: isProduction ? ['https://onehundredletters.com'] : ['http://localhost:3000'], // Change URL based on environment
     },
     upload: {
-      target: 'temporary-public-storage',
+      target: 'temporary-public-storage', // You can upload to a dashboard if you want persistent results
     },
   },
 };
