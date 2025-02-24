bf83abcb7cf383fe18f54b2b7507edb39e56c4a3
Jesse Mull <jessemull@gmail.com>
2025-02-23 16:22:46 -0800

    feat: added linting and test coverage for accessibility (#21)
    Co-authored-by: Jesse Mull <jessemull@Jesses-Laptop.local>

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,5 +1,12 @@
 import HomePage from './page';
+import { axe } from 'jest-axe';
 import { render, screen } from '@testing-library/react';
+
+jest.mock('next/link', () => {
+  const MockLink = ({ children }: { children: React.ReactNode }) => <>{children}</>;
+  MockLink.displayName = 'Link';
+  return MockLink;
+});
 
 describe('HomePage Component', () => {
   it('Renders homepage.', () => {
@@ -7,5 +14,11 @@ describe('HomePage Component', () => {
     expect(screen.getByText('100 Letters Project')).toBeInTheDocument();
     expect(screen.getByText('© 2025 100 Letters Project. All rights reserved.')).toBeInTheDocument();
     expect(screen.getByText('Letters')).toBeInTheDocument();
+  });
+
+  it('Has no accessibility errors.', async () => {
+    const { container } = render(<HomePage />);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
   });
 });
