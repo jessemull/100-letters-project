5271e8f9a734fb9754291f04c4fb6cb07adb8459
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-02-23 22:23:19 -0800

    feat: added Google Analytics scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import Script from 'next/script';
+import { Geist, Geist_Mono } from 'next/font/google';
+import type { Metadata } from 'next';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -12,6 +13,8 @@ const geistMono = Geist_Mono({
   subsets: ['latin'],
 });
 
+const GA_TRACKING_ID = 'G-T56NPJ5SBE';
+
 export const metadata: Metadata = {
   title: '100 Letters Project',
   description: '100 letters, 100 people, 1 year.',
@@ -22,12 +25,30 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const isProduction = process.env.NODE_ENV === 'production';
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
+        {isProduction && (
+          <>
+            <Script
+              strategy="afterInteractive"
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            />
+            <Script
+              id="google-analytics"
+              strategy="afterInteractive"
+            >
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){window.dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_TRACKING_ID}');
+              `}
+            </Script>
+          </>
+        )}
       </body>
     </html>
   );
