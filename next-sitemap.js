f0c7c20cfe8c749dcfbf3a6c8335407d3ab1188f
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-02-24 20:38:02 -0800

    fix: try using next-sitemap package

---
+++ next-sitemap.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: 'https://www.onehundredletters.com',
+  generateRobotsTxt: true,
+};
--- src/app/sitemap.ts
+++
@@ -1,8 +0,0 @@
-export default function sitemap() {
-  return [
-    {
-      url: 'https://onehundredletters.com',
-      lastModified: new Date().toISOString(),
-    },
-  ];
-}
