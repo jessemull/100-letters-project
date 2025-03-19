466f11ea21d7b7e4433278da2323f93484dfa31d
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-03-19 14:32:15 -0700

    fix: fix URLs for cypress and lighthouse

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   e2e: {
-    baseUrl: process.env.NODE_ENV === 'production' ? 'https://www.onehundredletters.com' : 'http://localhost:3000',
+    baseUrl:
+      process.env.NODE_ENV === 'production'
+        ? 'https://onehundredletters.com'
+        : 'https://dev.onehundredletters.com',
     supportFile: false,
   },
 };
