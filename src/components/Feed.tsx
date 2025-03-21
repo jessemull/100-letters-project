86d30529daec95cff1d3555e722123bdc2447a38
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-03-21 13:20:19 -0700

    fix: display coming soon message

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -6,13 +6,34 @@ import { useContext } from 'react';
 
 const Feed = () => {
   const { correspondences } = useContext(CorrespondenceContext);
+
   return (
-    <main className="bg-gray-100 py-12">
-      <div className="container mx-auto">
+    <main className="bg-gray-100 py-12 min-h-[calc(100vh-120px)] flex flex-col justify-between">
+      <div className="container mx-auto flex-grow">
         <div className="space-y-8">
-          {correspondences.map((item: Correspondence) => (
-            <Card key={item.correspondenceId} correspondence={item} />
-          ))}
+          {correspondences.length === 0 ? (
+            <>
+              <p className="text-center text-xl text-gray-500 font-bold">
+                Coming Soon...
+              </p>
+              <p className="text-center text-l text-gray-500 w-1/2 mx-auto">
+                The <strong>100 Letters Project</strong> is driven by the desire
+                to promote real world human interaction in an increasingly
+                digital landscape and create meaningful connections through
+                handwritten communication. Over the course of a year, I will
+                write 100 letters to 100 individuals.
+              </p>
+              <p className="text-center text-l text-gray-500 w-1/2 mx-auto">
+                This website will showcase these exchanges, offering a digital
+                display of the letters with details about the recipients and the
+                reasons behind their selection.
+              </p>
+            </>
+          ) : (
+            correspondences.map((item: Correspondence) => (
+              <Card key={item.correspondenceId} correspondence={item} />
+            ))
+          )}
         </div>
       </div>
     </main>
