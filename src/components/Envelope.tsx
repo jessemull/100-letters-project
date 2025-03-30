c4723c947e4cba2b6d73903b55ae95ba4e752a78
Jesse Mull <jessemull@gmail.com>
2025-03-30 13:05:04 -0700

    refactor: just make spacing a bit more between hearts (#65)
    Co-authored-by: Jesse Mull <jessemull@Jesses-Laptop.local>

--- src/components/Envelope.tsx
+++ src/components/Envelope.tsx
@@ -10,10 +10,10 @@ interface EnvelopeProps {
 }
 
 const heartsConfig = [
-  { delay: 1.5, baseOffsetY: 0.05, baseSize: 24 },
-  { delay: 0, baseOffsetY: 0.275, baseSize: 32 },
+  { delay: 1.5, baseOffsetY: 0.15, baseSize: 24 },
+  { delay: 0, baseOffsetY: 0.4, baseSize: 32 },
   { delay: 2, baseOffsetY: 0.05, baseSize: 36 },
-  { delay: 1, baseOffsetY: 0.175, baseSize: 24 },
+  { delay: 1, baseOffsetY: 0.25, baseSize: 24 },
 ];
 
 const Envelope: React.FC<EnvelopeProps> = ({ containerRef }) => {
