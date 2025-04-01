b9ae7ab4a22bb22dd264ad5183c2d5780f5f192f
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-04-01 16:05:23 -0700

    fix: fix region

--- cloudfront/index.ts
+++ cloudfront/index.ts
@@ -5,7 +5,7 @@ import { decode, JwtPayload, verify, VerifyErrors } from 'jsonwebtoken';
 import { logger } from './logger';
 
 const ADMIN_PATH_REGEX = /^\/admin(\/.*)?$/;
-const JWKS_URI = `https://cognito-idp.us-east-1.amazonaws.com/${COGNITO_USER_POOL_ID}/.well-known/jwks.json`;
+const JWKS_URI = `https://cognito-idp.us-west-2.amazonaws.com/${COGNITO_USER_POOL_ID}/.well-known/jwks.json`;
 
 const client = jwksClient({ jwksUri: JWKS_URI });
 
