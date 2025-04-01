08d17c871d75014ac2a6a967f279f4b49b722605
Jesse Mull <jessemull@gmail.com>
2025-04-01 10:56:49 -0700

    Lambda at edge (#68)
    * feat: add lambda at edge for auth and deep linking
    
    * fix: fix env names and logger names
    
    * fix: fix names
    
    * fix: fix types and add deploy pipeline
    
    * fix: fix pipeline
    
    * fix: try to exclude cloudfront
    
    * fix: ignore cloudfront tests
    
    * fix: fix pipeline
    
    ---------
    
    Co-authored-by: Jesse Mull <jessemull@Jesses-Laptop.local>

---
+++ cloudfront/index.ts
@@ -0,0 +1,82 @@
+import jwksClient from 'jwks-rsa';
+import { CloudFrontRequestEvent, CloudFrontRequestResult } from 'aws-lambda';
+import { decode, Jwt, JwtPayload, verify, VerifyErrors } from 'jsonwebtoken';
+import { logger } from './logger';
+
+const ADMIN_PATH_REGEX = /^\/admin(\/.*)?$/;
+const COGNITO_USER_POOL_CLIENT_ID = process.env.COGNITO_USER_POOL_CLIENT_ID;
+const COGNITO_USER_POOL_ID = process.env.COGNITO_USER_POOL_ID;
+const JWKS_URI = `https://cognito-idp.${process.env.AWS_REGION}.amazonaws.com/${COGNITO_USER_POOL_ID}/.well-known/jwks.json`;
+
+const client = jwksClient({ jwksUri: JWKS_URI });
+
+async function getSigningKey(kid: string): Promise<string> {
+  const key = await client.getSigningKey(kid);
+  return key.getPublicKey();
+}
+
+async function verifyToken(token: string): Promise<JwtPayload | null> {
+  const decoded = decode(token, { complete: true });
+
+  if (!decoded || typeof decoded === 'string' || !decoded.header.kid) {
+    throw new Error('Invalid JWT');
+  }
+
+  const signingKey = await getSigningKey(decoded.header.kid);
+
+  return new Promise((resolve, reject) => {
+    verify(
+      token,
+      signingKey,
+      { algorithms: ['RS256'], complete: true },
+      (err: VerifyErrors | null, payload: JwtPayload | undefined) => {
+        if (err) return reject(err);
+
+        if (!payload || typeof payload === 'string')
+          return reject(new Error('Invalid payload'));
+
+        if (payload.aud !== COGNITO_USER_POOL_CLIENT_ID) {
+          return reject(new Error('Invalid audience'));
+        }
+
+        resolve(payload);
+      },
+    );
+  });
+}
+
+export const handler = async (
+  event: CloudFrontRequestEvent,
+): Promise<CloudFrontRequestResult> => {
+  const request = event.Records[0].cf.request;
+  const headers = request.headers;
+  const uri = request.uri;
+
+  if (!ADMIN_PATH_REGEX.test(uri)) {
+    return request;
+  }
+
+  const authHeader = headers['authorization']?.[0]?.value;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return {
+      status: '403',
+      statusDescription: 'Forbidden',
+      body: 'Access denied!',
+    };
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    await verifyToken(token);
+    return request;
+  } catch (error) {
+    logger.error('JWT Verification Failed: ', error);
+    return {
+      status: '403',
+      statusDescription: 'Forbidden',
+      body: 'Access denied!',
+    };
+  }
+};
