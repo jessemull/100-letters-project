e12ae58bff0b065e3203768505f9dc7b1f83a2c3
Jesse Mull <jessemull@gmail.com>
2025-04-04 15:48:10 -0700

    fix: some clean up and pin eslint (#81)
    Co-authored-by: Jesse Mull <jessemull@Jesses-Laptop.local>

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 const ComingSoon = () => (
-  <div
-    className={`font-merriweather pt-4 flex flex-col items-center text-center space-y-4`}
-  >
+  <div className="font-merriweather pt-4 flex flex-col items-center text-center space-y-4">
     <p className="text-2xl text-gray-500 font-bold">Coming Soon...</p>
     <p className="text-lg text-gray-500 md:w-2/3 lg:w-2/3 xl:w-1/2 md:mx-auto">
       The <strong>100 Letters Project</strong> is driven by the desire to
