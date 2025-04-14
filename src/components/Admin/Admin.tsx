239742e52743ab8d1864c50067940cb4d7cc0bb6
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-04-13 20:21:49 -0700

    style: fix styles for admin section

--- src/components/Admin/Admin.tsx
+++ src/components/Admin/Admin.tsx
@@ -88,13 +88,13 @@ const Admin = () => {
   return (
     <div className="p-4 bg-fixed bg-center bg-[repeating-linear-gradient(45deg,_blue_0,_blue_16px,_red_16px,_red_32px,_white_32px,_white_48px)] min-h-screen w-full">
       <div className="flex w-full bg-white text-gray-800 font-merriweather min-h-[calc(100vh-16px)]">
-        <div className="w-full py-4 px-4 sm:py-16 sm:px-24">
+        <div className="w-full py-4 px-4">
           <div className="sm:hidden mb-6 sm:mb-4">
             <label htmlFor="tab-select" className="sr-only">
               Select a section
             </label>
             <select
-              className="w-full pl-3 pr-3 py-3 rounded-xl border-2 border-black shadow-sm bg-white text-xl font-bold appearance-none"
+              className="w-full pl-3 pr-3 py-3 rounded-xl border border-black shadow-sm bg-white font-bold appearance-none"
               id="tab-select"
               onChange={(e) => setSelectedIndex(Number(e.target.value))}
               value={selectedIndex}
@@ -107,10 +107,10 @@ const Admin = () => {
             </select>
           </div>
           <TabGroup selectedIndex={selectedIndex} onChange={setSelectedIndex}>
-            <TabList className="hidden sm:flex space-x-2 mb-6 border-b-2 border-black">
+            <TabList className="hidden sm:flex space-x-2 mb-6 border-b border-black">
               {tabs.map((tab) => (
                 <Tab
-                  className={`px-4 py-2 font-semibold text-xl transition ${
+                  className={`px-4 py-2 font-semibold text-lg transition ${
                     tab.key === tabs[selectedIndex].key
                       ? 'font-bold'
                       : 'text-gray-500 hover:text-gray-800'
@@ -124,12 +124,12 @@ const Admin = () => {
             <div className="mb-6">
               <div className="relative w-full">
                 <Search
-                  className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-700 w-6 h-6 sm:w-5 sm:h-5"
+                  className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-700 w-5 h-5"
                   strokeWidth={3}
                 />
                 <input
                   data-testid="admin-search"
-                  className="w-full pl-10 pr-3 py-3 rounded-xl border-2 border-black shadow-sm bg-white placeholder-gray-500 text-xl sm:text-xl"
+                  className="w-full pl-10 pr-3 py-3 rounded-xl border border-black shadow-lg bg-white placeholder-gray-500 text-md"
                   onChange={(e) => setSearch(e.target.value)}
                   placeholder={activeTab.placeholder}
                   type="text"
