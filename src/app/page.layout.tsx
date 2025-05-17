3b833eea62f92d20536a6ec5e591d0c37c6827f0
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-05-17 13:28:07 -0700

    fix: fix layout for mobile

--- src/app/page.layout.tsx
+++ src/app/page.layout.tsx
@@ -17,38 +17,51 @@ const PageLayout = ({ children }: { children: React.ReactNode }) => {
   );
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col min-h-screen relative">
       <div
-        className="fixed inset-0 -z-10 bg-cover bg-center"
+        className="min-h-screen fixed inset-0 -z-10 bg-cover bg-center"
         style={{
           backgroundImage:
             "linear-gradient(rgba(0,0,0,0.75), rgba(0,0,0,0.75)), url('/signin.webp')",
           backgroundAttachment: 'fixed',
         }}
       />
-      <Header />
-      <div className="flex w-full">
-        <div
-          className={`hidden lg:block ${sidebarWidth} text-white`}
-          data-testid="menu-width"
-        >
-          <div className="flex flex-col h-screen sticky top-0">
-            <div
-              className="flex-1 overflow-y-auto overflow-x-hidden"
-              style={{ scrollbarGutter: 'stable' }}
-            >
-              <SearchProvider>
-                <DesktopMenu
-                  collapsed={collapsed}
-                  setCollapsed={setCollapsed}
-                />
-              </SearchProvider>
+      <div className="relative z-10 flex flex-col min-h-screen">
+        <Header />
+        <div className="hidden lg:flex w-full flex-1">
+          <div
+            className={`${sidebarWidth} text-white`}
+            data-testid="menu-width"
+          >
+            <div className="flex flex-col h-screen sticky top-0">
+              <div
+                className="flex-1 overflow-y-auto overflow-x-hidden"
+                style={{ scrollbarGutter: 'stable' }}
+              >
+                <SearchProvider>
+                  <DesktopMenu
+                    collapsed={collapsed}
+                    setCollapsed={setCollapsed}
+                  />
+                </SearchProvider>
+              </div>
             </div>
           </div>
+          <main className="flex-1 p-8">{children}</main>
         </div>
-        <main className="flex-1 p-4 sm:p-6 md:p-8">{children}</main>
+        <main
+          className={`
+            flex lg:hidden flex-1 justify-center items-center
+            p-4 sm:p-6 md:p-8
+            min-h-[calc(100vh-56px-56px-32px)]
+            sm:min-h-[calc(100vh-56px-56px-24px)]
+            min-h-[calc(100vh-56px-56px-16px)]
+          `}
+        >
+          {children}
+        </main>
+        <Footer />
       </div>
-      <Footer />
       <Toaster position="top-center" />
     </div>
   );
