176d474aa2ca2d714bd8508b44f7d51e4e002575
Jesse Mull <jessemull@gmail.com>
2025-05-26 20:37:21 -0700

    feat: add blurred placeholder image for loading (#127)
    Co-authored-by: Jesse Mull <jessemull@Jesses-Laptop.local>

--- src/components/Admin/Letters/Image.tsx
+++ src/components/Admin/Letters/Image.tsx
@@ -38,6 +38,8 @@ const Image = ({
       onError={() => setImgSrc(fallbackSrc)}
       sizes={sizes}
       priority={priority}
+      placeholder="blur"
+      blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNzIwIiBoZWlnaHQ9IjQwMCIgdmlld0JveD0iMCAwIDcyMCA0MDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGZpbHRlciBpZD0iYiI+PGZlR2F1c3NpYW5CbHVyIHN0ZERldmlhdGlvbj0iMTIiIC8+PC9maWx0ZXI+PHJlY3Qgd2lkdGg9IjcyMCIgaGVpZ2h0PSI0MDAiIGZpbHRlcj0idXJsKCNiKSIgZmlsbD0idXJsKCNnKSIgLz48bGluZWFyR3JhZGllbnQgaWQ9ImciIHgxPSIwJSIgeTE9IjAlIiB4Mj0iMTAwJSIgeTI9IjEwMCUiPjxzdG9wIG9mZnNldD0iMCUiIHN0b3AtY29sb3I9IiNmZmYiIHN0b3Atb3BhY2l0eT0iMC4yIi8+PHN0b3Agb2Zmc2V0PSIxMDAlIiBzdG9wLWNvbG9yPSIjY2NjY2NjIiBzdG9wLW9wYWNpdHk9IjAuMiIvPjwvbGluZWFyR3JhZGllbnQ+PC9zdmc+"
       src={imgSrc || fallbackSrc}
       width={width}
     />
