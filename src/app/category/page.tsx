0418efe04d232848fc32943362f8502d65d897d2
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-05-30 16:30:42 -0700

    fix: add suspense boundary

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -1,11 +1,15 @@
 import PageLayout from '../page.layout';
 import { Category } from '@components/Category';
+import { Progress } from '@components/Form';
 import { SearchProvider } from '@contexts/SearchProvider';
+import { Suspense } from 'react';
 
 const CategoryPage = () => (
   <PageLayout>
     <SearchProvider>
-      <Category />
+      <Suspense fallback={<Progress size={16} color="white" />}>
+        <Category />
+      </Suspense>
     </SearchProvider>
   </PageLayout>
 );
