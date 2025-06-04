bd7e22cf5dca12214f3d8626ba8db1efe9406f9d
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-06-04 12:22:57 -0700

    fix: use server instrumentation for dev server only

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,12 +1,11 @@
 import * as Sentry from '@sentry/nextjs';
 
 export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
+  const isDev = process.env.NODE_ENV === 'development';
+  const isNode = process.env.NEXT_RUNTIME === 'nodejs';
+  const isExport = process.env.NEXT_PHASE === 'phase-export';
+  if (isDev && isNode && !isExport) {
     await import('../sentry.server.config');
-  }
-
-  if (process.env.NEXT_RUNTIME === 'edge') {
-    await import('../sentry.edge.config');
   }
 }
 
