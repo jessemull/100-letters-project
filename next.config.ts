07acaa524b185b55d53e96af4ed8316666aedaa2
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-06-07 19:02:13 -0700

    fix: fix source maps

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import type { NextConfig } from 'next';
 
 const baseConfig: NextConfig = {
   output: 'export',
-  productionBrowserSourceMaps: false,
+  productionBrowserSourceMaps: true,
   images: {
     unoptimized: true,
   },
