1a660f131f5184e67fecb40abe45442944beceb0
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-06-12 21:09:38 -0700

    fix: updated messaging

--- src/components/ComingSoon/ComingSoon.tsx
+++ src/components/ComingSoon/ComingSoon.tsx
@@ -4,9 +4,12 @@ const ComingSoon = () => (
   <div className="font-merriweather flex flex-col items-center text-center space-y-4">
     <p className="text-md md:text-lg text-white md:w-2/3 lg:w-2/3 xl:w-1/2 md:mx-auto">
       The <strong>100 Letters Project</strong> is driven by the desire to
-      promote real-world human interaction in an increasingly digital landscape
-      and create meaningful connections through handwritten communication. Over
-      the course of a year, I will write 100 letters to 100 individuals.
+      promote human interaction in an increasingly digital landscape.
+    </p>
+    <p className="text-md md:text-lg text-white md:w-2/3 lg:w-2/3 xl:w-1/2 md:mx-auto">
+      Over the course of a year, I will write 100 letters to 100 individuals.
+      Each letter will be a personal reflection, written by hand and sent with
+      intention and love.
     </p>
     <p className="text-md md:text-lg text-white md:w-2/3 lg:w-2/3 xl:w-1/2 md:mx-auto">
       This website will showcase these exchanges, offering a digital display of
