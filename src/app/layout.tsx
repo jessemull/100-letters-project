978d657146cc3c3fe4150853eb100b619a731ff9
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-07-23 08:45:50 -0700

    style: linter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata = {
   alternates: {
     canonical: 'https://www.onehundredletters.com',
   },
-  description: 
+  description:
     '100 letters written to 100 individuals over the course of one year. Each letter is a personal reflection, handwritten and sent with intention and love.',
   metadataBase: new URL('https://www.onehundredletters.com'),
   openGraph: {
